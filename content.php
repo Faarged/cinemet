2eb7d196255f78f05b09b7a7d47014cb23673df9
lilian <l.stoklosa@simplon-charleville.fr>
2019-04-26 08:30:54 +0200

    utilise la base de données pour remplis le site et se rend compte de bugs liés aux affiches

--- content.php
+++ content.php
@@ -59,57 +59,41 @@
 
     <!-- pour l'image du film -->
     <div class="media shadow-lg p-3 mb-5 bg-light rounded">
-      <img src="..." class="mr-3" alt="...">
+      <img src="<?php echo $donnees['affiche']; ?>" class="mr-3">
     </div>
 
     <!-- pour la description du film -->
 
-    <p class="text-center bg-light">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-      minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
-      esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-      non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-      minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
-      esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-      non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-    </p>
+    <p class="text-center bg-light"><?php echo $donnees['resume']; ?></p>
 
     <!-- pour la partie récap d'infos et la bande annonce -->
 
   <div class="row">
     <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
-
-    <div class="list-group">
-  <a href="realisateur.html" class="list-group-item list-group-item-action">
-    <div class="d-flex w-100 justify-content-between">
-      <h5 class="mb-1">Réalisateur</h5>
+      <div class="list-group">
+        <a href="realisateur.html" class="list-group-item list-group-item-action">
+          <div class="d-flex w-100 justify-content-between">
+            <h5 class="mb-1">Réalisateur</h5>
+          </div>
+          <p class="mb-1"><?php echo $donnees['realisateur']; ?>
+        </a>
+        <a href="#" class="list-group-item list-group-item-action">
+          <div class="d-flex w-100 justify-content-between">
+            <h5 class="mb-1">Durée</h5>
+          </div>
+          <p class="mb-1"><?php echo $donnees['duree']; ?>
+        </a>
+        <a href="acteur.html" class="list-group-item list-group-item-action">
+          <div class="d-flex w-100 justify-content-between">
+            <h5 class="mb-1">Acteur vedette</h5>
+          </div>
+          <p class="mb-1"><?php echo $donnees['acteur']; ?>
+        </a>
+      </div>
     </div>
-    <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
-    <small>Donec id elit non mi porta.</small>
-  </a>
-  <a href="#" class="list-group-item list-group-item-action">
-    <div class="d-flex w-100 justify-content-between">
-      <h5 class="mb-1">Production</h5>
-    </div>
-    <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
-    <small class="text-muted">Donec id elit non mi porta.</small>
-  </a>
-  <a href="acteur.html" class="list-group-item list-group-item-action">
-    <div class="d-flex w-100 justify-content-between">
-      <h5 class="mb-1">Acteurs</h5>
-    </div>
-    <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
-    <small class="text-muted">Donec id elit non mi porta.</small>
-  </a>
-</div>
-</div>
 
   <div class="col-xs-12 col-sm-12 col-md-8 col-lg-8">
-    <iframe class="shadow-lg p-3 mb-5 bg-light rounded" src="" height="100%" width="100%"></iframe>
+    <iframe class="shadow-lg p-3 mb-5 bg-light rounded" src="<?php echo $donnees['trailer']; ?>" height="100%" width="100%"></iframe>
   </div>
 
 </div> <!-- fin row-->
