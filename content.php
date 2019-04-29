8c4930665ca3d08c3d76ec1242cce39bc026bae7
lilian <l.stoklosa@simplon-charleville.fr>
2019-04-29 08:57:14 +0200

    améliore lisibilité du code

--- content.php
+++ content.php
@@ -105,11 +105,14 @@
 <?php include 'footer.php'; ?>
 
   <div><a id="cRetour" class="cInvisible" href="#content"></a></div>
+
+  <!-- ************Je ferme ma requete vers la BDD************** -->
 <?php }
 
 $reponse->closeCursor(); // Termine le traitement de la requête
 
 ?>
+
 <!-- jQuery first, then Popper.js, then Bootstrap JS -->
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
