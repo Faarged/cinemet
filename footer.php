bfc5d3e34392141dae85aef84825a89416d2daf6
lilian <l.stoklosa@simplon-charleville.fr>
2019-04-29 08:04:22 +0200

    modifie le footer en reprenant le code d'un footer sur mon github

--- footer.php
+++ footer.php
@@ -1,123 +1,53 @@
-<footer id="footer" class="text-white">
-
-    <!-- Footer Links -->
-    <div class="container text-center">
-
-        <!-- Footer links -->
-        <div class="row">
-
-            <!-- Grid column -->
-            <div class="col-sm-12 col-md-3 col-lg-3 col-xl-3">
-                <h5 class=" text-uppercase mb-4  font-weight-bold text-white"><a href="index.php"> AllocineMET</a>
-                </h5>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                    labore et dolore magna aliqua.</p>
-            </div>
-            <!-- Grid column -->
-
-            <hr class="w-100">
-
-            <!-- Grid column -->
-            <div class="col-sm-3 col-md-2 col-lg-2 col-xl-2" id="foot">
-                <h5 class="text-uppercase font-weight-bold">Films à l'affiche</h5>
-                <p>
-                    <a href="#">Film 1</a>
-                </p>
-                <p>
-                    <a href="#">Film 2</a>
-                </p>
-                <p>
-                    <a href="#">Film 3</a>
-                </p>
-                <p>
-                    <a href="#">Film 4</a>
-                </p>
-            </div>
-            <!-- Grid column -->
-
-            <hr class="w-100" id="foot">
-
-            <!-- Grid column -->
-            <div class="col-sm-3 col-md-3 col-lg-2 col-xl-2" id="foot">
-                <h5 class="text-uppercase font-weight-bold">Liens utiles</h5>
-                <p>
-                    <a href="https://simplon.co/">Simplon</a>
-                </p>
-                <p>
-                    <a href="https://simplon-charleville.fr/">Simplon Charleville</a>
-                </p>
-                <p>
-                    <a href="#">Lorem Ipsum</a>
-                </p>
-                <p>
-                    <a href="#">Lorem Ipsum</a>
-                </p>
-            </div>
-
-            <!-- Grid column -->
-            <hr class="w-100" id="foot">
-
-            <!-- Grid column -->
-            <div class="col-sm-6 col-md-4 col-lg-3 col-xl-3">
-                <h5 class="text-uppercase font-weight-bold"><a href="contact.php">Contact</a></h5>
-                <p>
-                    AllocineMET</p>
-                <p>
-                    www.AllocineMET.net</p>
-                <p>
-                    TEL +33 6 52 50 05 35</p>
-                <p>
-                    TEL +33 6 87 26 69 70</p>
-            </div>
-            <!-- Grid column -->
-
-        </div>
-        <!-- Footer links -->
-
-        <hr class="hr-footer">
-
-        <!-- Grid row -->
-        <div class="row d-flex align-items-center">
-
-            <!-- Grid column -->
-            <div class="col-md-6 col-lg-7">
-
-                <!--Copyright-->
-                <p class="text-center text-md-left">© 2019 Copyright: AllocineMET
-
-                </p>
-
-            </div>
-            <!-- Grid column -->
-
-            <!-- Grid column -->
-            <div class="col-md-6 col-lg-5 ml-lg-0">
-
-                <!-- Social buttons -->
-                <div class="text-center text-md-right">
-                    <ul class="list-unstyled list-inline">
-                        <li class="list-inline-item">
-                            <a class="btn-floating btn-sm rgba-white-slight mx-1" href="https://www.facebook.com/">
-                                <i class="fab fa-facebook-f"></i></a>
-                        </li>
-                        <li class="list-inline-item">
-                            <a class="btn-floating btn-sm rgba-white-slight mx-1" href="https://twitter.com/">
-                              <i class="fab fa-twitter"></i></a>
-                        </li>
-                        <li class="list-inline-item">
-                            <a class="btn-floating btn-sm rgba-white-slight mx-1" href="https://github.com/">
-                              <i class="fab fa-github"></i></a>
-                        </li>
-                    </ul>
-                </div>
-
-            </div>
-            <!-- Grid column -->
-
-        </div>
-        <!-- Grid row -->
-
+<footer id="footer">
+  <div class="container-fluid">
+    <div class="row une">
+      <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+        <ul>
+          <li><a href="index.php">Accueil</a></li>
+          <li><a href="formation.php">Nos Films</a></li>
+          <li><a href="candidate.php">Je candidate</a></li>
+          <li><a href="#">Filmographie</a></li>
+        </ul>
+      </div>
+      <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+        <ul>
+          <li><a href="#">Conditions</a></li>
+          <li><a href="#">Politique de confidentialité</a></li>
+          <li><a href="#">Sécurité</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </div>
+      <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+        <ul>
+          <li><a href="#">6 Rue Longueville</a></li>
+          <li><a href="#">08000 Charleville-Mézières</a></li>
+          <li><a href="#">metropolis@mail.net</a></li>
+          <li><a href="#">0324332208</a></li>
+        </ul>
+      </div>
     </div>
-    <!-- Footer Links -->
-
+    <div class="row taille">
+      <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12"></div>
+      <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+        <ul class="list-inline">
+          <li class="list-inline-item">
+            <a class="btn-floating mx-1" href="https://www.facebook.com/">
+              <i class="fab fa-facebook-f"></i></a>
+          </li>
+          <li class="list-inline-item">
+            <a class="btn-floating mx-1" href="https://twitter.com/">
+              <i class="fab fa-twitter"></i></a>
+          </li>
+          <li class="list-inline-item">
+            <a class="btn-floating mx-1" href="https://github.com/">
+              <i class="fab fa-github"></i></a>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="row deux">
+      <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+        <p>©2019 Copyright: AllocineMET. Tous droits réservés</p>
+      </div>
+    </div>
 </footer>
