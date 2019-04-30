05da8d4247a5e7522ed46f746c242c31850e8024
lilian <l.stoklosa@simplon-charleville.fr>
2019-04-30 09:33:50 +0200

    modif nav responsive mais burger caché et fonctionnel, ajout carousel

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
       <link rel="stylesheet" href="style/reset.css">
       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
       <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+      <link href='https://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
       <link rel="stylesheet" href="style/index.css">
       <link rel="stylesheet" href="style/header.css">
       <link rel="stylesheet" href="style/footer.css">
@@ -60,6 +61,16 @@
         </div>
       </div>
 
+        <div class="Container">
+          <ul class="Carousel">
+            <li class="Items Front"><img src="img/3.jpg" class="w-100" alt="..."></li>
+            <li class="Items Left"><img src="img/7.jpg" class="w-100" alt="..."></li>
+            <li class="Items Left2"><img src="img/9.jpg" class="w-100" alt="..."></li>
+            <li class="Items Right"><img src="img/1.jpg" class="w-100" alt="..."></li>
+            <li class="Items Right2"><img src="img/5.jpg" class="w-100" alt="..."></li>
+          </ul>
+        </div>
+
 
       <!--**************************parallax*********************** -->
       <div class="parallax-window" data-parallax="scroll" data-image-src="img/shining.jpg"></div>
@@ -73,5 +84,23 @@
       <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
       <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
       <script src="js/parallax.js"></script>
+      <script>
+        $(function(){
+          var front = $('.Front'),
+              others = ['Left2', 'Left', 'Right', 'Right2'];
+
+          $('.Carousel').on('click', '.Items', function() {
+            var $this = $(this);
+
+            $.each(others, function(i, cl) {
+              if ($this.hasClass(cl)) {
+                front.removeClass('Front').addClass(cl);
+                front = $this;
+                front.addClass('Front').removeClass(cl);
+              }
+            });
+          });
+        });
+      </script>
     </body>
   </html>
