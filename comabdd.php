170b93dfb1b8ed5d5adf2328ee4d74e14b2b7aeb
lilian <l.stoklosa@simplon-charleville.fr>
2019-05-02 08:18:31 +0200

    co a ma bdd sur mon pc perso en localhost, je vais voir à le faire sur la vrai bdd

--- comabdd.php
+++ comabdd.php
@@ -2,7 +2,7 @@
 try
 {
 	// On se connecte à MySQL
-$bdd = new PDO('mysql:host=localhost;dbname=phpmyadmin;charset=utf8', 'phpmyadmin', 'pioupiou', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION)); /*'mysql:host=db776288365.hosting-data.io;dbname=film;charset=utf8', 'dbo776288365', 'w67wj898h'*/
+$bdd = new PDO('mysql:host=localhost;dbname=phpmyadmin;charset=utf8', 'lilian', '21septembre', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION)); /*'mysql:host=db776288365.hosting-data.io;dbname=film;charset=utf8', 'dbo776288365', 'w67wj898h'*/
 }
 catch(Exception $e)
 {
