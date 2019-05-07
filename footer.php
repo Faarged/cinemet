1b90dee2a7e4aa26a74e4728c5d5754e06430e2f
lilian <l.stoklosa@simplon-charleville.fr>
2019-05-07 08:25:40 +0200

    ajoute une animation sur l'arrivée du footer

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-<footer id="footer">
+<footer class="animated slideInRight" id="footer">
   <div class="container-fluid">
     <div class="row une">
       <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
