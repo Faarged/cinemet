423f2a437a28ee14bf50bb3f89f579c680def16b
lilian <l.stoklosa@simplon-charleville.fr>
2019-05-17 15:07:57 +0200

    essaie pr jointure non concluant

--- ajout.php
+++ ajout.php
@@ -5,15 +5,6 @@ header('Location: admin.php');
 //connexion à ma bdd en utilisant la page comabdd
 require 'php/comabdd.php';
 
-// Insertion du message à l'aide d'une requête préparée
-try {
-  if(!empty($_POST['titre']) AND !empty($_POST['duree']) AND !empty($_POST['affiche']) AND !empty($_POST['date_sortie']) AND !empty($_POST['resume']) AND !empty($_POST['trailer']) AND !empty($_POST['realisateur']) AND !empty($_POST['acteur'])){
-    echo "<h1>Entrées validées</h1><br />";
-  }else{
-    echo "<h1>Champs non remplis</h1><br />";
-  }
-
-
   if(isset($_POST['titre'], $_POST['duree'], $_POST['affiche'], $_POST['date_sortie'], $_POST['resume'], $_POST['trailer'], $_POST['realisateur'], $_POST['acteur'], $_POST['genre'])){
     if(isset($_POST['realisateur2'])){
       $realisateur= $_POST['realisateur2'];
@@ -43,12 +34,20 @@ try {
       'resume' => $resume,
       'trailer' => $trailer
     ));
+    $verif = $bdd->prepare('SELECT nom_real FROM realisateur');
+    $verif->execute();
+    while($real = $verif->fetch()){
+      if($realisateur == $real['nom_real']){
+
+
+      }
+    }
 
     $req2 = $bdd->prepare('INSERT INTO realisateur(nom_real)
     VALUES(:nom_real)');
     $req2 ->execute(array(
       'nom_real' => $realisateur
-    ));
+      ));
 
     $req3 = $bdd->prepare('INSERT INTO acteur(nom_acteur)
     VALUES(:nom_acteur)');
